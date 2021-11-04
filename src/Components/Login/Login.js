8c8726d65b2284d28e56cbbb7c71c6daa6533573
DiegoMurcia2022 <diegomurciac2220@gmail.com>
2021-11-03 21:44:13 -0500

    Login in tests

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,35 +1,43 @@
+import { Avatar, Button, CardMedia, Container, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import axios from "axios";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useHistory } from "react-router";
+import LockOutlined from "@mui/icons-material/LockOutlined";
 
 const Login = (props) => {
   const emailRef = useRef();
   const passwordRef = useRef();
-
   const history = useHistory();
 
   const loginRequest = () => {
-    axios.post("http://localhost:8080/v1/auth", {
-        email: emailRef.current.value,
-        password: passwordRef.current.value
-    }).then(response => {
-        console.log(response)
-        history.push("/dashboard")
+    axios.post("https://onlyfit-backend-staging.herokuapp.com/v1/auth", {
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }).then((res) => {
+      localStorage.setItem("accessToken", res.data.accessToken)
+      history.push("/dashboard")
     }).catch(error => {
-        alert(error)
+      alert(error)
     });
   };
 
   return (
     <div>
-      <h1>Login!!</h1>
-      <div>
-        <label>email</label>
-        <input ref={emailRef} />
-        <label>password</label>
-        <input type="password" ref={passwordRef} />
-        <button onClick={loginRequest}>Login</button>
-      </div>
+      <Container component="main" maxWidth="xs">
+        <CardMedia component="img" image="/static/images/logo_onlyfit.png" />
+        <Box sx={{ marginTop: -6, display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">Log in</Typography>
+          <Box component="form" sx={{ mt: 1 }}>
+            <TextField margin="normal" required fullWidth label="Email" name="email" type="email" autoComplete="email" inputRef={emailRef} />
+            <TextField margin="normal" required fullWidth label="Password" name="password" type="password" autoComplete="current-password" inputRef={passwordRef} />
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 3 }} onClick={loginRequest}>Log in</Button>
+          </Box>
+        </Box>
+      </Container>
     </div>
   );
 };
