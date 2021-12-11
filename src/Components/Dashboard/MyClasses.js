71a8cf1c6239b7c1a9bd1b280a9b15971645d1f4
SoyTiyi <santiago.marmartinez@gmail.com>
2021-12-10 23:25:01 -0500

    final push

--- src/Components/Dashboard/MyClasses.js
+++ src/Components/Dashboard/MyClasses.js
@@ -11,74 +11,83 @@ import {
   Button,
   Box,
   AppBar,
-} from "@mui/material";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import DashboardNavBar from "./DashboardNavBar";
+} from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import DashboardNavBar from './DashboardNavBar'
 
 const MyClasses = () => {
-  const [myRoutines, setMyRoutines] = useState([]);
-  const email = localStorage.getItem("email");
-  const [test, setTest] = useState({});
+  const [myRoutines, setMyRoutines] = useState([])
+  const email = localStorage.getItem('email')
+  const [test, setTest] = useState([])
 
   useEffect(() => {
     function getMyRoutines() {
       axios
         .get(
-          `https://onlyfit-backend-staging.herokuapp.com/v1/user/routines?emailAddress=${email}`
+          `https://onlyfit-backend-staging.herokuapp.com/v1/user/routines?emailAddress=${email}`,
         )
         .then((res) => {
-          setMyRoutines(res.data);
-        });
+
+          res.data.map((element) => {
+            axios
+              .get(
+                `https://onlyfit-backend-staging.herokuapp.com/v1/routine/${element}`,
+              )
+              .then((response) => {
+                console.log(response)
+                setTest((prevArray) => [...prevArray, {
+                  id: response.data.id,
+                  name: response.data.name,
+                  description: response.data.description
+                }])
+              })
+              .catch((error) => {
+                console.log(error)
+                alert('Error traer rutina rutina')
+              })
+          })      
+        })
+        .catch((error) => {
+          alert('Error traer ids')
+        })
     }
 
-    getMyRoutines();
-  }, []);
+    getMyRoutines()
+  }, [])
+
+
+  console.log(test,"TEST")
 
   let data = (
     <Grid container spacing={3}>
-      <Grid item xs={12} style={{ paddingTop: "5%", marginLeft: "2%" }}>
+      <Grid item xs={12} style={{ paddingTop: '5%', marginLeft: '2%' }}>
         <Alert severity="info">
           <AlertTitle>INFORMACION</AlertTitle>
-          No hay rutinas asignadas en este pefil —{" "}
+          No hay rutinas asignadas en este pefil —{' '}
           <strong>agrega una rutina o comunicate con servicio tecnico</strong>
         </Alert>
       </Grid>
     </Grid>
-  );
+  )
 
-  function getInfo(ele) {
-    axios
-      .get(`https://onlyfit-backend-staging.herokuapp.com/v1/routine/${ele}`)
-      .then((res) => {
-        setTest(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("error");
-      });
-  }
-
-  if (myRoutines.length > 0) {
-    data = myRoutines.map((ele, index) => {
+  if (test.length > 0) {
+    data = test.map((ele, index) => {
       var imageName;
-      var routine;
 
-      getInfo(ele);
-
-      if (test.name === "cardio") {
+      if (ele.name === "cardio") {
         imageName =
           "https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=248.88&h=140&fit=crop&auto=format";
-      } else if (test.name === "crossfit") {
+      } else if (ele.name === "crossfit") {
         imageName =
           "https://images.unsplash.com/photo-1517964603305-11c0f6f66012?w=248.88&h=140&fit=crop&auto=format";
-      } else if (test.name === "pesas") {
+      } else if (ele.name === "pesas") {
         imageName =
           "https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?w=248.88&h=140&fit=crop&auto=format";
-      } else if (test.name === "tenis") {
+      } else if (ele.name === "tenis") {
         imageName =
           "https://www.ecestaticos.com/imagestatic/clipping/0ce/50c/0ce50c2c82d99851bcd9e3b4d4ef0a9d/para-que-es-bueno-practicar-tenis-y-las-posibles-lesiones-que-debes-vigilar.jpg?w=248.88&h=140&fit=crop&auto=format";
-      } else if (test.name === "zumba") {
+      } else if (ele.name === "zumba") {
         imageName =
           "https://static2.abc.es/media/bienestar/2021/01/19/zumba-kUnF--620x349@abc.jpg?w=248.88&h=140&fit=crop&auto=format";
       }
@@ -90,14 +99,14 @@ const MyClasses = () => {
               component="img"
               height="140"
               image={imageName}
-              alt={test.id}
+              alt={ele.id}
             />
             <CardContent>
               <Typography gutterBottom variant="h4" component="div">
-                {test.name}
+                {ele.name}
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                {test.description}
+                {ele.description}
               </Typography>
             </CardContent>
             <CardActions>
@@ -117,7 +126,7 @@ const MyClasses = () => {
         {data}
       </Grid>
     </div>
-  );
-};
+  )
+}
 
-export default MyClasses;
+export default MyClasses
